547aa2aeb92a729677f28d7f7af45f2a8acd5c70
R P <ashwinkumar.rp@cognizant.com>
2020-01-22 17:15:45 +0530

    Changes

--- Controller/BibleController.js
+++ Controller/BibleController.js
@@ -5,10 +5,8 @@ var {Title} = require('../model/heading');
 const bible =  require('../bible.json');
 
 router.get('/:title',(req,res)=>{
-    console.log(req.params.title);
     Book.find({title : req.params.title},(err,doc)=>{
         if(!err){
-            console.log(doc);
             res.send(doc);
         }
         else{
@@ -27,16 +25,4 @@ router.get('/',(req,res)=>{
     });
 });
 
-router.get('/title/:title/chapterno/:chap/no/:no',(req,res)=>{
-    Book.find({title : req.params.title},(err,doc)=>{
-        if(!err){
-            res.send(doc[0].Chapter[req.params.chap].Verse[req.params.no]);
-        }
-        else{
-            console.log(err);
-        }
-    });
-});
-
-router.get
 module.exports = router;
