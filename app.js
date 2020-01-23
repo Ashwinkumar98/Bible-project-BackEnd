2474420936ba2eaaf0694c677248a492569e6f50
R P <ashwinkumar.rp@cognizant.com>
2020-01-23 17:50:39 +0530

    changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bibleController = require('./Controller/BibleController.js');
 var app= express();
 var port =  process.env.PORT || 5000
 app.use(bodyparser.json());
-// app.use(cors({origin:"http://localhost:3001"}));
+app.use(cors({origin:"http://localhost:3001"}));
 app.use('/bible',bibleController);
 app.listen(port,()=>console.log('server started at port '+port));
 
