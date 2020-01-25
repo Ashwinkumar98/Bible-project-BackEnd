606a8b911993100b20a1a4e385fb8bf18e141cc9
Ashwin Kumar R P <rpaswin008@gmail.com>
2020-01-25 22:14:48 +0530

    Added

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const cors = require('cors');
 const {mongoose} =  require('./db.js');
 const bibleController = require('./Controller/BibleController.js');
 var app= express();
-var port =  process.env.PORT || 5000
+var port =  process.env.PORT || 3000
 app.use(bodyparser.json());
-app.use(cors({origin:"http://localhost:3001"}));
+app.use(cors({origin:"https://bibleapp-front-end.herokuapp.com/"}));
 app.use('/bible',bibleController);
 app.listen(port,()=>console.log('server started at port '+port));
 
