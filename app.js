df550c7cccc596093d9047995249e211a43cb11e
Ashwin Kumar R P <rpaswin008@gmail.com>
2020-01-26 18:45:08 +0530

    all allows

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const bibleController = require('./Controller/BibleController.js');
 var app= express();
 var port =  process.env.PORT || 3000
 app.use(bodyparser.json());
-app.use(cors({origin:"https://bibleapp-front-end.herokuapp.com/*"}));
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+   });
 app.use('/bible',bibleController);
 app.listen(port,()=>console.log('server started at port '+port));
 
