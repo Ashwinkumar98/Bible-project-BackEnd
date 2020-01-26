e8a5fb2ca90a41e62f58cddeadbb1110da8340bd
Ashwin Kumar R P <rpaswin008@gmail.com>
2020-01-26 19:07:30 +0530

    Cros update

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const bodyparser = require('body-parser');
-const cors = require('cors');
+
 const {mongoose} =  require('./db.js');
 const bibleController = require('./Controller/BibleController.js');
 var app= express();
 var port =  process.env.PORT || 3000
 app.use(bodyparser.json());
-app.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
     next();
-   });
+  });
 app.use('/bible',bibleController);
 app.listen(port,()=>console.log('server started at port '+port));
 
